07feeaa781578a5e4541a67c9f47f7432af40245
cuiqijia <1097993489@qq.com>
2019-07-14 21:37:19 +0800

    绘制表单

--- src/view/login/index.vue
+++ src/view/login/index.vue
@@ -1,38 +1,61 @@
 <template>
-    <div class="login-container">
-        <el-card class="login-box">
-            <img src="../../assets/images/logo_index.png" alt="">
-        </el-card>
-    </div>
+  <div class="login-container">
+    <el-card class="login-box">
+      <img src="../../assets/images/logo_index.png" alt />
+      <!-- 登录表单 -->
+      <el-form ref="loginForm" :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
+        <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+             <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+            <el-button style="width:100%" type="primary">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
 <style scoped lang='less'>
-.login-container{
-    width:100%;
-    height: 100%;
+.login-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  //cover 铺满容器 多余的裁剪  contain 在容器内完全显示图片
+  background: url(../../assets/images/hai.jpg) no-repeat center / cover;
+  .login-box {
+    width: 400px;
+    height: 330px;
     position: absolute;
-    left: 0;
-    top: 0;
-    //cover 铺满容器 多余的裁剪  contain 在容器内完全显示图片
-    background: url(../../assets/images/hai.jpg) no-repeat center / cover;
-    .login-box{
-        width: 400px;
-        height: 300px;
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%,-50%);
-        img{
-            display: block;
-            width: 200px;
-            margin:10px auto;
-        }
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 10px auto;
     }
+  }
 }
 </style>
