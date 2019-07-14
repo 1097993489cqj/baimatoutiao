8d6d9ec3df0c2abe700f55290aed00b95ad0a706
cuiqijia <1097993489@qq.com>
2019-07-14 11:16:38 +0800

    baima

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 导包
+import ElementUI from 'element-ui'
+// 导样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 注册实例
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
